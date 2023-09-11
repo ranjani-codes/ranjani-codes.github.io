e4ac57f80255b046305e638a14c7b0d97f55f4e8
Ranjani <ranjani.venkataramanit@gmail.com>
2023-09-11 21:32:32 +0200

    setup pages react

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           }}
         >
           {/*<img src={logo} alt="logo" className="w-9 h-9 object-contain" />*/}
-          <p className="text-white text-[18px] font-normal cursor-pointer flex">
+          <p className="text-white text-[24px] font-normal cursor-pointer flex">
             ranjani<span className="font-extrabold sm:block hidden">Codes</span>
           </p>
         </Link>
