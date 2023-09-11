68a81523665dcba066cb9da86ceb340d9985a676
Ranjani <ranjani.venkataramanit@gmail.com>
2023-09-11 22:04:12 +0200

    setup pages react

---
+++ webpack.config.cjs
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.jsx', // Your main JSX file
+  output: {
+    filename: 'bundle.js', // Output bundle file name
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
