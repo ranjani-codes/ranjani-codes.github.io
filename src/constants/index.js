7d09dff826fd6574333dd6abb5416e22d59563ff
Ranjani <ranjani.venkataramanit@gmail.com>
2024-06-16 21:58:28 +0200

    update app 06/24

--- src/constants/index.js
+++ src/constants/index.js
@@ -117,30 +117,30 @@ const technologies = [
 
 const experiences = [
   {
-    title: "Data Scientist Consultant",
+    title: "Data Scientist",
     company_name: "Rhenus Warehousing Solutions",
     icon: "",
     iconBg: "#383E56",
     date: "October 2023 - Present",
     points: [
-      "Conducting thorough stakeholder interviews and in-depth analyses to gain a deep understanding of the logistics processes and business pain points.",
-      "Prototyping machine learning models for potential use in areas such as route optimization, demand forecasting, and inventory management, providing valuable insights into feasibility.",
-      "Developing data visualization prototypes to illustrate the potential impact of machine learning on logistics operations.",
+      "Creating a workload forecasting model from scratch based on stakeholder requirements, logistic processes, and existing internal infrastructure.",
+      "Developing dashboards for topics such as sustainability and SPQDC to track KPIs and key metrics to aid in data-driven decision making.",
+      "Prototyping machine learning models for potential use in areas such as demand forecasting, process optimisation, and inventory management, providing valuable insights into functionality and scalability.",
       "Working closely with internal teams to assess the readiness of existing infrastructure for future machine learning model deployment.",
     ],
     tools: [
-      "Tools/Languages Used: : Python, PowerBI, Excel, JIRA, Azure"
+      "Tools/Languages Used: : Python, PowerBI, DAX, Excel, JIRA, Azure, Databricks"
     ]
   },
   {
-    title: "Freelance Consultant",
+    title: "Data Scientist Consultant",
     company_name: " ",
     icon: "",
     iconBg: "#383E56",
     date: "April 2023 - October 2023",
     points: [
-      "Developing a full-stack web application with seamless integration of a mobile app, enabling users to access and interact with the platform across multiple devices.",
-      "Analysing and predicting future outcomes using company finance data. Providing actionable recommendations to high-level stakeholders and presenting insights through visualizations.",
+      "Developing a full-stack web application with functionality across all devices, enabling users to access and interact with the platform seamlessly.",
+      "Analysing and predicting future outcomes using company finance data. Providing actionable recommendations to high-level stakeholders and presenting insights through visualisations.",
     ],
     tools: [
       "Tools/Languages Used: : Python, SQL, PowerBI, HTML, CSS, Javascript, React, Django"
@@ -154,7 +154,7 @@ const experiences = [
     date: "Sep 2022 - Feb 2023",
     points: [
       "Gaining insights on client usage patterns through a data-driven dashboard.",
-      "Utilising Machine learning and implementing ETL solutions to facilitate cross-functional analysis of usage data and product optimization across departments.",
+      "Utilising Machine learning and implementing ETL solutions to facilitate cross-functional analysis of usage data and product optimisation across departments.",
       "Leveraging data-driven insights through the utilisation of Big Query, Google Analytics, Microsoft Excel, and Amplitude to drive the progress of underdeveloped projects.",
       "Taking charge of a project from end to end using Jira to track the progress.",
     ],
@@ -169,9 +169,9 @@ const experiences = [
     iconBg: "#383E56",
     date: "Feb 2022 - July 2022",
     points: [
-      "Providing support to the Senior Data Analyst in PowerBI-based projects aimed at delivering visualized results to clients.",
+      "Providing support to the Senior Data Analyst in PowerBI-based projects aimed at delivering visualised results to clients.",
       "Working on Data Processing and Data Visualisation in Python with libraries such as Sklearn and Seaborn.",
-      "Utilizing the Python Pandas library to clean and process data, in order to enhance the accuracy of analysis and improve the quality of decision-making",
+      "Utilising the Python Pandas library to clean and process data, in order to enhance the accuracy of analysis and improve the quality of decision-making",
     ],
     tools:[
       "Tools/Languages Used: PowerBI, Python, Jupyter, Anaconda"
@@ -213,7 +213,7 @@ const testimonials = [
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      "After Rick optimised our website, our traffic increased by 50%. We can't thank them enough!",
     name: "Lisa Wang",
     designation: "CTO",
     company: "456 Enterprises",
@@ -222,6 +222,130 @@ const testimonials = [
 ];
 
 const projects = [
+  
+  {
+    name: "Workload Forecasting",
+    short_description: "Deep Learning Algorithm that learns demand trends and generates forecasts for individual clients",
+    long_description:
+      "A deep learning algorithm trained on a dataset of daily order quantity data, combined with generated features, capable of discerning patterns and generating order volume forecasts, thereby optimising workforce planning",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ]
+  },
+  {  
+    name: "Sustainability Dashboard",
+    short_description: "A dashboard of sustainability indicators which enables data-driven decision-making leading to enhanced sustainable processes",
+    long_description:
+      "A dashboard that provides real-time visualisations of sustainability indicators such as energy consumption, waste reduction, and resource efficiency, enabling data-driven decision-making to promote sustainable practices.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ]
+  },
+  {
+    name: "SPQDC Dashboard",
+    short_description: "A dashboard that enhances warehouse operations by monitoring KPIs and key metrics in Safety, People, Quality, Delivery, and Cost",
+    long_description:
+      "A KPI Dashboard that enhances warehouse operations by monitoring KPIs and key metrics in Safety, People, Quality, Delivery, and Cost, leading to improved performance and efficiency.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ]
+  },
+  {
+    name: "Container Quantity Planning",
+    short_description: "Graph Based Algorithm that generates container quantity requirements based on order portfolios and inventory data",
+    long_description:
+      "A graph-based algorithm to generate precise container requirements by analysing order portfolios and inventory data. It's designed to streamline operations, improve planning, and ensure optimal resource allocation for businesses in need of continuous container utilisation",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ]
+  },
+  {             
+    name: "Client Conversion Predictor",
+    short_description: "A Machine-Learning model using regression techniques that identifies leads that could convert to clients",
+    long_description:
+      "A combination-regression model that uses data on client demographics, usage history, frequency of usage, and other relevant information to predict the likelihood of client conversions and client churn.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+  },
+  {             
+    name: "Movie Recommender System",
+    short_description: "A Recommender System that suggests movies to users based on similarities between users and similarities between items",
+    long_description:
+      "A Recommender System that employs User-Based Collaborative Filtering and Item-Based Collaborative Filtering approaches to suggest movies to individual users based on their preferences, including previously unseen ones, and also offers group recommendations based on the collective preferences of the members.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/ranjani-codes/Movie-Recommender-System",
+  },
   {
     name: "Information Retrieval System",
     short_description: "Natural Language Processing Algorithm that aims to optimise customer service requests",
@@ -248,7 +372,7 @@ const projects = [
     name: "Trade Signal Generator",
     short_description: "Trading bot that generates Trading Signals using Operations Research techniques",
     long_description:
-      "A model that employs a Markov Decision Process (MDP) in combination with Policy Iteration to determine optimal trading signals by analyzing historical data, enabling it to assess whether a given stock is likely to yield profitable returns based on past price trends.",
+      "A model that employs a Markov Decision Process (MDP) in combination with Policy Iteration to determine optimal trading signals by analysing historical data, enabling it to assess whether a given stock is likely to yield profitable returns based on past price trends.",
     tags: [
       {
         name: "react",
@@ -267,10 +391,10 @@ const projects = [
     source_code_link: "",
   },
   {
-    name: "AI-Based Personal Trainer",
-    short_description: "Reinforcement Learning based Algorithm that learns training patterns and customizes them to match individual user goals",
+    name: "Personal Trainer Bot",
+    short_description: "Reinforcement Learning based Algorithm that learns workout patterns and customises training plans to match individual user goals",
     long_description:
-      "A machine learning algorithm trained on a vast dataset of training plans designed by coaches for their clients, capable of discerning patterns and generating personalized plans for individual users.",
+      "A machine learning algorithm trained on a vast dataset of training plans designed by coaches for their clients, capable of discerning patterns and generating personalised plans for individual users.",
     tags: [
       {
         name: "nextjs",
@@ -289,9 +413,9 @@ const projects = [
   },
   {             
     name: "Personal Assistant Chatbot",
-    short_description: "An AI-based PA that assists you in planning your days based on your schedule, complete with faec recognition and calendar sync",
+    short_description: "An AI-based PA that assists users in planning your days based on their schedule, complete with facial recognition and calendar sync",
     long_description:
-      "A facial recognition-enabled chatbot that utilizes Image and Video Processing for face recognition and Natural Language Processing for interpreting user queries. The system can optimize your schedule by integrating with linked calendars and setting reminders automatically based on the type of event.",
+      "A facial recognition-enabled chatbot that utilises Image and Video Processing for face recognition and Natural Language Processing for interpreting user queries. The system can optimise your schedule by integrating with linked calendars and setting reminders automatically based on the type of event.",
     tags: [
       {
         name: "nextjs",
@@ -309,10 +433,9 @@ const projects = [
     image: tripguide,
     source_code_link: "https://github.com/ranjani-codes/AI-Personal-Assistant",
   },
-
   {             
     name: "Employee Scheduler",
-    short_description: " Python-based algorithm to aid business owners in shift scheduling",
+    short_description: "Rule-based algorithm to aid business owners in shift scheduling",
     long_description:
       "An algorithm that schedules shifts for employees by factoring in individual availability, rank, responsibilities, and employer specifications, with plans to develop a user-friendly interface and offer it as a service to clients.",
     tags: [
@@ -331,50 +454,7 @@ const projects = [
     ],
     image: tripguide,
     source_code_link: "",
-  },
-  {             
-    name: "Movie Recommender System",
-    short_description: "A Recommender System that suggests movies to users based on similarities between users and similarities between items",
-    long_description:
-      "A Recommender System that employs User-Based Collaborative Filtering and Item-Based Collaborative Filtering approaches to suggest movies to individual users based on their preferences, including previously unseen ones, and also offers group recommendations based on the collective preferences of the members.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tripguide,
-    source_code_link: "https://github.com/ranjani-codes/Movie-Recommender-System",
-  },
-  {             
-    name: "Client Conversion Predictor",
-    short_description: "A Machine-Learning model using regression techniques that identifies leads that could convert to clients",
-    long_description:
-      "A combination-regression model that uses data on client demographics, usage history, frequency of usage, and other relevant information to predict the likelihood of client conversions and client churn.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tripguide,
-  },
+  }
 ];
 
 export { services, technologies, experiences, testimonials, projects };
